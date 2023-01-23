7c6d5c9be4402f8d45762a836fb9a938670b0658
Neil Martey <neil.marteybotchway@gmail.com>
2023-01-23 10:40:00 +0000

    Added quiz mode and practice mode

--- main_quiz/urls.py
+++ main_quiz/urls.py
@@ -8,4 +8,6 @@ app_name = 'main_quiz'
 urlpatterns = [
     #Defining the URL pattern used to access the homepage, once this URL has been entered, the fucntion index() from the file views will be called. Name attribute allows us to refrence this URL in other sections of code.
     path('', views.index, name='index'),
+    path('practice-mode/', views.practice_mode, name='practice_mode'),
+    path('quiz-mode/', views.quiz_mode, name='quiz_mode')
 ]
--- main_quiz/views.py
+++ main_quiz/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import TimesTable
 
 def index(request):
     """The home page for mulipleChoice multiplication. Pass request object as parameter to render function, template as other parameter"""
     return render(request, 'main_quiz/index.html')
+
+def practice_mode(request):
+    """Show all times tables."""
+    times_tables = TimesTable.objects.all()
+    context = {'times_tables': times_tables}
+    return render(request, 'main_quiz/practice_mode.html', context)
+
+def quiz_mode(request):
+    """Determine quiz with lowest percentage, and show this to the user."""
+    weakest_times_table = TimesTable.objects.order_by('-average_percentage')[11]
+    context = {'weakest_times_table': weakest_times_table}
+    return render(request, 'main_quiz/quiz_mode.html', context)
+

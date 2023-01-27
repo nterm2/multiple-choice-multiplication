829e6c4f8fb9264115613237aa6f226999ce5058
Neil Martey <neil.marteybotchway@gmail.com>
2023-01-27 22:37:23 +0000

    Added functionality to save percentage on quiz

--- main_quiz/views.py
+++ main_quiz/views.py
@@ -21,25 +21,28 @@ def times_table(request, times_table_id):
     """Show a times table and all of the questions within that times table."""
     # Get individual times table object
     times_table = TimesTable.objects.get(id=times_table_id)
+    current_percentage = times_table.average_percentage
     # Get questions associated with the specific times table
     questions = Question.objects.filter(times_table__pk=times_table_id)
+
+    if request.method == 'POST':
+        # Retrieve list answers from users form when they submit it
+        user_answers = dict(request.POST)
+        del user_answers['csrfmiddlewaretoken']
+        user_answers = user_answers.values()
+        user_answers = sum(user_answers, [])
+        # Now they can be compared against the answers for the questions, and a percentage can be calculated.
+        score = 0
+        for i in range(12):
+            user_answer = user_answers[i]
+            actual_answer = questions[i].answer
+            if int(user_answer) == actual_answer:
+                score += 1
+        percentage = int((score / 12) * 100)
+        updated_average = int((current_percentage + percentage) / 2)
+        times_table.average_percentage = updated_average
+        times_table.save()
+
     context = {'times_table': times_table, 'questions': questions}
-
-    # Retrieve list answers from users form when they submit it
-    user_answers = dict(request.POST)
-    del user_answers['csrfmiddlewaretoken']
-    user_answers = user_answers.values()
-    user_answers = sum(user_answers, [])
-
-    # Now they can be compared against the answers for the questions, and a percentage can be calculated.
-    score = 0
-    for i in range(12):
-        user_answer = user_answers[i]
-        actual_answer = questions[i].answer
-        if int(user_answer) == actual_answer:
-            score += 1
-    percentage = int((score / 12) * 100)
-    print(percentage)
-
 
     return render(request, 'main_quiz/times_table.html', context)

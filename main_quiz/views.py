1c210708766727ffa7d3ff5856c0ae3b85017d04
Neil Martey <neil.marteybotchway@gmail.com>
2023-01-28 07:55:22 +0000

    Restrict access to pages requring login

--- main_quiz/views.py
+++ main_quiz/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
 from .models import TimesTable, Question
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     """The home page for mulipleChoice multiplication. Pass request object as parameter to render function, template as other parameter"""
     return render(request, 'main_quiz/index.html')
 
+@login_required
 def practice_mode(request):
     """Show all times tables."""
     times_tables = TimesTable.objects.all()
     context = {'times_tables': times_tables}
     return render(request, 'main_quiz/practice_mode.html', context)
 
+@login_required
 def quiz_mode(request):
     """Determine quiz with lowest percentage, and show this to the user."""
     weakest_times_table = TimesTable.objects.order_by('-average_percentage')[11]
     context = {'weakest_times_table': weakest_times_table}
     return render(request, 'main_quiz/quiz_mode.html', context)
 
+@login_required
 def times_table(request, times_table_id):
     """Show a times table and all of the questions within that times table."""
     # Get individual times table object

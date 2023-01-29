6060aefc8b1015c7942f121f96a7826f44dd4a79
Neil Martey <neil.marteybotchway@gmail.com>
2023-01-29 08:14:07 +0000

    Included Question Overview model

--- main_quiz/admin.py
+++ main_quiz/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Import the TimesTable model that was created
-from .models import TimesTable, Question
+from .models import TimesTable, Question, QuestionOverview
 
 # Allows admin to manage the model through the admin site.
 admin.site.register(TimesTable)
 admin.site.register(Question)
+admin.site.register(QuestionOverview)
--- main_quiz/models.py
+++ main_quiz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 class TimesTable(models.Model):
     """Represents the values associated for a given times table."""
@@ -8,6 +9,43 @@ class TimesTable(models.Model):
     def __str__(self):
         """Return a string representation of the TimesTable model"""
         return f'Times Table: {self.times_table} Average Percentage: {self.average_percentage}%'
+
+class QuestionOverview(models.Model):
+    """Model reprsenting question overview for a student"""
+    one_times_table = models.IntegerField(default=1)
+    one_avg = models.IntegerField(default=0)
+
+    two_times_table = models.IntegerField(default=2)
+    two_avg = models.IntegerField(default=0)
+
+    three_times_table = models.IntegerField(default=3)
+    three_avg = models.IntegerField(default=0)
+    
+    four_times_table = models.IntegerField(default=4)
+    four_avg = models.IntegerField(default=0)
+    
+    five_times_table = models.IntegerField(default=5)
+    five_avg = models.IntegerField(default=0)
+    
+    six_times_table = models.IntegerField(default=6)
+    six_avg = models.IntegerField(default=0)
+    
+    seven_times_table = models.IntegerField(default=7)
+    seven_avg = models.IntegerField(default=0)
+    
+    eight_times_table = models.IntegerField(default=8)
+    eight_avg = models.IntegerField(default=0)
+    
+    nine_times_table = models.IntegerField(default=9)
+    nine_avg = models.IntegerField(default=0)
+    
+    ten_times_table = models.IntegerField(default=10)
+    ten_avg = models.IntegerField(default=0)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.owner.username}'
+
 
 class Question(models.Model):
     """Represents a question within a quiz"""
